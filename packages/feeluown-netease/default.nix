f22de56cff307ac2e948e5bf818b2f2717088f33
Potato Hatsue <1793913507@qq.com>
2021-01-25 23:43:17 +0800

    Manual update

--- packages/feeluown-netease/default.nix
+++ packages/feeluown-netease/default.nix
@@ -3,15 +3,16 @@
 let inherit (pythonPackages) buildPythonPackage fetchPypi;
 
 in buildPythonPackage rec {
+  inherit (mySource) pname version src;
 
-  pname = "fuo_netease";
-  version = "0.4.4";
+  # pname = "fuo_netease";
+  # version = "0.4.4";
 
-  # mySource is not used, because the latest version supports only the alpha core
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "AR94sP4e1dadm+ytj+GyXBnLDsGykrayHaQSx3GY3Ac=";
-  };
+  # # mySource is not used, because the latest version supports only the alpha core
+  # src = fetchPypi {
+  #   inherit pname version;
+  #   sha256 = "AR94sP4e1dadm+ytj+GyXBnLDsGykrayHaQSx3GY3Ac=";
+  # };
 
   doCheck = false;
 
