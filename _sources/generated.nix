5a102dabce996e7d0c4913e0805dbc3e9b8d5fb1
berberman <berberman@users.noreply.github.com>
2021-08-23 10:16:40 +0000

    fcitx5-pinyin-zhwiki: 0.2.3.20210804 → 0.2.3.20210823

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -39,10 +39,10 @@
   };
   fcitx5-pinyin-zhwiki = {
     pname = "fcitx5-pinyin-zhwiki";
-    version = "0.2.3.20210804";
+    version = "0.2.3.20210823";
     src = fetchurl {
-      url = "https://github.com/felixonmars/fcitx5-pinyin-zhwiki/releases/download/0.2.3/zhwiki-20210804.dict";
-      sha256 = "0fa3za236ravvmvjk0xl7n3n2m4fpk1svwc8sxz94pvaylxc4vac";
+      url = "https://github.com/felixonmars/fcitx5-pinyin-zhwiki/releases/download/0.2.3/zhwiki-20210823.dict";
+      sha256 = "1kys7nhnavvmaaw32rpyh10n1p892nyz5k2rx30fv44cbimb2mdk";
     };
   };
   feeluown-core = {
