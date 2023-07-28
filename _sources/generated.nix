9d676bee73974a2e0fbe03aa9015445d8caa682d
berberman <berberman@users.noreply.github.com>
2023-07-28 06:13:13 +0000

    fcitx5-pinyin-zhwiki: 0.2.4.20230605 → 0.2.4.20230728

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -38,10 +38,10 @@
   };
   fcitx5-pinyin-zhwiki = {
     pname = "fcitx5-pinyin-zhwiki";
-    version = "0.2.4.20230605";
+    version = "0.2.4.20230728";
     src = fetchurl {
-      url = "https://github.com/felixonmars/fcitx5-pinyin-zhwiki/releases/download/0.2.4/zhwiki-20230605.dict";
-      sha256 = "sha256-G44bgOWpnQEbP78idcOobEUm2m+7cYM+UCqyJu+D+9E=";
+      url = "https://github.com/felixonmars/fcitx5-pinyin-zhwiki/releases/download/0.2.4/zhwiki-20230728.dict";
+      sha256 = "sha256-m7bmyvdgUrBR+1uCRmWDc5LAiVbrnqc//Hl4iNbTkGk=";
     };
   };
   feeluown-bilibili = {
